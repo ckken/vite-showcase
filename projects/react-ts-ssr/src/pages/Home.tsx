7a0e2de86b48a9143a37e2bd9d5336fcfacb0e79
xuhongbin <xuhongbin@yy.com>
2021-04-29 17:11:18 +0800

    up

--- projects/react-ts-ssr/src/pages/Home.tsx
+++ projects/react-ts-ssr/src/pages/Home.tsx
@@ -1,3 +1,12 @@
+
 export default function Home() {
+  if(typeof window!=='undefined'){
+
+    (async()=>{
+      const confetti = await import('https://cdn.skypack.dev/canvas-confetti?dts')
+       confetti.default()
+    })()
+  }
+
   return <h1>Home</h1>
 }
